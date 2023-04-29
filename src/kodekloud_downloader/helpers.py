1bfeb45c59960fa1d5df1f88cfcce077d365eade
Roy, Debakar <debakar.roy@intel.com>
2023-04-29 10:32:01 +0530

    Adding docstring and logic to check duplicate video

--- src/kodekloud_downloader/helpers.py
+++ src/kodekloud_downloader/helpers.py
@@ -1,3 +1,4 @@
+import hashlib
 import logging
 import string
 from pathlib import Path
@@ -13,6 +14,12 @@ logger = logging.getLogger(__name__)
 
 
 def select_course(courses: List[Course]) -> Course:
+    """
+    Display a table of courses and ask the user to select one by entering its number.
+
+    :param courses: A list of Course objects to choose from
+    :return: The selected Course object
+    """
     table = prettytable.PrettyTable()
     table.field_names = ["No.", "Name", "Type", "Categories"]
 
@@ -35,10 +42,24 @@ def select_course(courses: List[Course]) -> Course:
 
 
 def normalize_name(name: str) -> str:
+    """
+    Remove punctuation from a string.
+
+    :param name: The input string
+    :return: The string without punctuation
+    """
     return name.translate(str.maketrans("", "", string.punctuation))
 
 
-def download_video(url: str, output_path: Path, cookie: str, quality: str) -> int:
+def download_video(url: str, output_path: Path, cookie: str, quality: str) -> None:
+    """
+    Download a video using yt_dlp with the given options.
+
+    :param url: The video URL
+    :param output_path: The output directory for the downloaded video
+    :param cookie: The user's authentication cookie
+    :param quality: The video quality (e.g. "720p")
+    """
     ydl_opts = {
         "format": f"bestvideo[height<={quality[:-1]}]+bestaudio/best[height<={quality[:-1]}]/best",
         "concurrent_fragment_downloads": 15,
@@ -46,28 +67,52 @@ def download_video(url: str, output_path: Path, cookie: str, quality: str) -> in
         "verbose": logger.getEffectiveLevel() == logging.DEBUG,
         "cookiefile": cookie,
         "merge_output_format": "mkv",
-        "writesubtitles": True
+        "writesubtitles": True,
     }
     logger.debug(f"Calling download with following options: {ydl_opts}")
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download(url)
 
 
-def normalize_name(name: str) -> str:
-    return name.translate(str.maketrans("", "", string.punctuation))
-
-
 def is_normal_content(content: str) -> bool:
+    """
+    Check if the content is not a lab or feedback.
+
+    :param content: The input content
+    :return: True if the content is normal, False otherwise
+    """
     is_lab = content.find("div", class_="start-lab-button")
     is_feedback = content.find_all("iframe")
     return not (is_lab or is_feedback)
 
 
 def download_all_pdf(content, download_path: Path, cookie: str) -> None:
+    """
+    Download all PDF files from the given content.
+
+    :param content: The content containing the PDF links
+    :param download_path: The output directory for the downloaded PDFs
+    :param cookie: The user's authentication cookie
+    """
     for link in content.find_all("a"):
         href = link.get("href")
         if href.endswith("pdf"):
             file_name = download_path / Path(href).name
-            print(f"Downloading {file_name}...")
+            logger.info(f"Downloading {file_name}...")
             response = requests.get(href, headers={"Cookie": cookie})
             file_name.write_bytes(response.content)
+
+
+def file_hash(file_path: Path, hash_algorithm: str = "sha256") -> str:
+    """
+    Calculate the hash of a file using the specified algorithm.
+
+    :param file_path: The path to the file
+    :param hash_algorithm: The hashing algorithm to use (default: 'sha256')
+    :return: The calculated hash as a hexadecimal string
+    """
+    hash_func = hashlib.new(hash_algorithm)
+    with open(file_path, "rb") as file:
+        while chunk := file.read(8192):
+            hash_func.update(chunk)
+    return hash_func.hexdigest()
--- src/kodekloud_downloader/main.py
+++ src/kodekloud_downloader/main.py
@@ -4,17 +4,20 @@ from typing import Union
 
 import markdownify
 import requests
-import kodekloud_downloader.yt_dlp_patch  # noqa
-import yt_dlp
 from bs4 import BeautifulSoup
 
+import kodekloud_downloader.yt_dlp_patch  # noqa
 from kodekloud_downloader.helpers import (
     download_all_pdf,
     download_video,
+    file_hash,
     is_normal_content,
     normalize_name,
 )
 from kodekloud_downloader.models import Topic
+
+import yt_dlp  # isort: skip
+
 
 logger = logging.getLogger(__name__)
 
@@ -22,54 +25,116 @@ logger = logging.getLogger(__name__)
 def download_course(
     url: str, cookie: str, quality: str, output_dir: Union[str, Path]
 ) -> None:
+    """
+    Download a course from KodeKloud.
+
+    :param url: The course URL
+    :param cookie: The user's authentication cookie
+    :param quality: The video quality (e.g. "720p")
+    :param output_dir: The output directory for the downloaded course
+    """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     course_name = soup.find("h1", class_="course_title").text.strip()
     main_lesson_content = soup.find("div", class_="lessons_main__content")
     topics = main_lesson_content.find_all("div", class_="w-dyn-item")
-    items = []
-    for topic in topics:
-        items.append(Topic.make(topic))
+    items = [Topic.make(topic) for topic in topics]
+
+    hash_of_download_videos = set()
     for i, item in enumerate(items, start=1):
         for j, lesson in enumerate(item.lessons, start=1):
-            file_path = Path(
-                Path(output_dir)
-                / "KodeKloud"
-                / normalize_name(course_name)
-                / f"{i} - {normalize_name(item.name)}"
-                / f"{j} - {normalize_name(lesson.name)}"
+            file_path = create_file_path(
+                output_dir, course_name, i, item.name, j, lesson.name
             )
+
             if lesson.is_video:
-                logger.info(f"Writing video file... {file_path}...")
-                file_path.parent.mkdir(parents=True, exist_ok=True)
-                logger.info(f"Parsing url: {lesson.url}")
-                try:
-                    download_video(
-                        url=lesson.url,
-                        output_path=file_path,
-                        cookie=cookie,
-                        quality=quality,
+                download_video_lesson(lesson, file_path, cookie, quality)
+                hash_of_download_video = file_hash(file_path.with_suffix(".mkv"))
+                if hash_of_download_video in hash_of_download_videos:
+                    logger.warning(
+                        "Your cookie might have expired or you don't have access to the course."
+                        "Please refresh/regenerate the cookie or enroll in the course and try again."
                     )
-                except yt_dlp.utils.UnsupportedError as ex:
-                    logger.error(
-                        f"Could not download video in link {lesson.url}. "
-                        "Please open link manually and verify that video exists!"
-                    )
-                except yt_dlp.utils.DownloadError as ex:
-                    logger.error(
-                        f"Access denied while downloading video or audio file from link {lesson.url}"
-                    )
+                    SystemExit(1)
+                hash_of_download_videos.add(hash_of_download_video)
             else:
-                page = requests.get(lesson.url)
-                soup = BeautifulSoup(page.content, "html.parser")
-                content = soup.find("div", class_="learndash_content_wrap")
-                # TODO: Fix this part. Most probably resources are not getting downloaded
-                if content and is_normal_content(content):
-                    logger.info(f"Writing resource file... {file_path}...")
-                    file_path.parent.mkdir(parents=True, exist_ok=True)
-                    file_path.with_suffix(".md").write_text(
-                        markdownify.markdownify(content.prettify()), encoding="utf-8"
-                    )
-                    download_all_pdf(
-                        content=content, download_path=file_path.parent, cookie=cookie
-                    )
+                download_resource_lesson(lesson, file_path, cookie)
+
+
+def create_file_path(
+    output_dir: Union[str, Path],
+    course_name: str,
+    i: int,
+    item_name: str,
+    j: int,
+    lesson_name: str,
+) -> Path:
+    """
+    Create a file path for a lesson.
+
+    :param output_dir: The output directory for the downloaded course
+    :param course_name: The course name
+    :param i: The topic index
+    :param item_name: The topic name
+    :param j: The lesson index
+    :param lesson_name: The lesson name
+    :return: The created file path
+    """
+    return Path(
+        Path(output_dir)
+        / "KodeKloud"
+        / normalize_name(course_name)
+        / f"{i} - {normalize_name(item_name)}"
+        / f"{j} - {normalize_name(lesson_name)}"
+    )
+
+
+def download_video_lesson(lesson, file_path: Path, cookie: str, quality: str) -> None:
+    """
+    Download a video lesson.
+
+    :param lesson: The lesson object
+    :param file_path: The output file path for the video
+    :param cookie: The user's authentication cookie
+    :param quality: The video quality (e.g. "720p")
+    """
+    logger.info(f"Writing video file... {file_path}...")
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    logger.info(f"Parsing url: {lesson.url}")
+    try:
+        download_video(
+            url=lesson.url,
+            output_path=file_path,
+            cookie=cookie,
+            quality=quality,
+        )
+    except yt_dlp.utils.UnsupportedError as ex:
+        logger.error(
+            f"Could not download video in link {lesson.url}. "
+            "Please open link manually and verify that video exists!"
+        )
+    except yt_dlp.utils.DownloadError as ex:
+        logger.error(
+            f"Access denied while downloading video or audio file from link {lesson.url}"
+        )
+
+
+def download_resource_lesson(lesson, file_path: Path, cookie: str) -> None:
+    """
+    Download a resource lesson.
+
+    :param lesson: The lesson object
+    :param file_path: The output file path for the resource
+    :param cookie: The user's authentication cookie
+    """
+    page = requests.get(lesson.url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    content = soup.find("div", class_="learndash_content_wrap")
+
+    if content and is_normal_content(content):
+        logger.info(f"Writing resource file... {file_path}...")
+        file_path.parent.mkdir(parents=True, exist_ok=True)
+        file_path.with_suffix(".md").write_text(
+            markdownify.markdownify(content.prettify()), encoding="utf-8"
+        )
+        download_all_pdf(content=content, download_path=file_path.parent, cookie=cookie)
